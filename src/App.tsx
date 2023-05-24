581cc73591415a8df23ebe32de6981115192ac38
ajayvardhan <ajyvardan@gmail.com>
2023-05-24 19:25:20 +0530

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import Form from "./components/Form/Form";
 
 const App: React.FC = () => {
   return (
     <div>
       <Form />
+      <Analytics />
     </div>
   );
 };
